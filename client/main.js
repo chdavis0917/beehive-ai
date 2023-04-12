f65207c42188cec9d2b5c4733598fe19f1731f82
chdavis0917 <chdavis0917@gmail.com>
2023-04-12 14:24:32 -0700

    refactored and simplified code

--- client/main.js
+++ client/main.js
@@ -1,92 +1,16 @@
-// Chatroom solution with WebSocket API and vanilla DOM manipulation
+let input = document.getElementById("input");
+let output = document.getElementById("output");
+let socket = new WebSocket("ws://localhost:8080/echo");
 
-// Variables to simplify references to HTML elements
-const submit = document.querySelector('#submit');
-const input = document.querySelector('input');
-const mainContainer = document.querySelector('#chatroom');
-
-// Create a new WebSocket object
-const ws = new WebSocket('wss://ih2ozbmr1j.execute-api.us-east-2.amazonaws.com/default/beehiveWebsockets');
-
-// Function for retrieving all messages
-const getMessages = () => {
-  // Send a message to the WebSocket server to retrieve all messages
-  ws.send(JSON.stringify({ action: 'getMessages' }));
+socket.onopen = function () {
+    output.innerHTML += "Status: Connected\n";
 };
 
-// Upon opening the WebSocket connection...
-ws.addEventListener('open', () => {
-    console.log("started up frontend");
-  getMessages();
-});
+socket.onmessage = function (e) {
+    output.innerHTML += "Server: " + e.data + "\n";
+};
 
-// Upon receiving a message from the WebSocket server...
-ws.addEventListener('message', (event) => {
-  const message = JSON.parse(event.data);
-
-  if (message && message.action === 'retrieveMessages') {
-    mainContainer.innerHTML = null;
-
-    for (let i = 0; i < message.data.length; i += 1) {
-        console.log("What kinds of messages are being sent?", message.data[i]);
-      new Message(message.data[i]);
-    }
-  }
-});
-
-// Function for posting a message
-submit.addEventListener('click', (e) => {
-  e.preventDefault();
-
-  if (input.value) {
-    console.log("What kinds of messages are being sent?", input.value);
-    // Send a message to the WebSocket server to post a new message
-    ws.send(
-      JSON.stringify({
-        action: 'postMessage',
-        data: {
-          message: input.value,
-          created_at: new Date(Date.now()),
-          created_by: 'Beehive'
-        },
-      })
-    );
-  }
-
-  input.value = '';
-});
-
-// Code for constructing an individual message component
-class Message {
-  constructor(msgData) {
-    this.message = msgData['message'];
-    this.author = msgData['created_by'];
-    this.timestamp = msgData['created_at'];
-
-    this.node = document.createElement('div');
-    this.node.setAttribute('class', 'message');
-    this.node.style.backgroundColor = 'whitesmoke';
-
-    const msgPara = document.createElement('p');
-    msgPara.innerText = `${this.author} at ${this.timestamp}: ${this.message}`;
-    msgPara.style.padding = '10px';
-    msgPara.style.marginTop = '5px';
-
-    this.node.appendChild(msgPara);
-    mainContainer.appendChild(this.node);
-  }
+function send() {
+    socket.send(input.value);
+    input.value = "";
 }
-
-// Styling
-document.querySelector('body').style.fontFamily = '"Trebuchet MS", Helvetica, sans-serif';
-
-mainContainer.style.height = '500px';
-mainContainer.style.width = '80%';
-mainContainer.style.margin = '30px auto 0 auto';
-mainContainer.style.border = '1px solid black';
-mainContainer.style.borderRadius = '5px';
-mainContainer.style.padding = '10px';
-mainContainer.style.overflowY = 'scroll';
-
-document.querySelector('input').style.width = '80%';
-document.querySelector('input').style.margin = '20px auto';
--- server/websocket_server.go
+++ server/websocket_server.go
@@ -1,108 +1,64 @@
 package main
 
 import (
-    "log"
-    "net/http"
-    "github.com/gorilla/mux"
-    "github.com/gorilla/websocket"
-    "encoding/json"
+	"fmt"
+	"net/http"
+
+	"github.com/gorilla/websocket"
 )
 
-type Message struct {
-    ID        string `json:"id,omitempty"`
-    Message   string `json:"message,omitempty"`
-    CreatedBy string `json:"created_by,omitempty"`
-}
-
-var messages []Message
-
 var upgrader = websocket.Upgrader{
-    ReadBufferSize:  1024,
-    WriteBufferSize: 1024,
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
 }
+
+var clients []websocket.Conn
+var history []string
 
 func main() {
-    router := mux.NewRouter()
+	http.HandleFunc("/echo", func(w http.ResponseWriter, r *http.Request) {
+		conn, _ := upgrader.Upgrade(w, r, nil)
 
-    // API endpoint to retrieve all messages
-    router.HandleFunc("/messages", getMessages).Methods("GET")
+		clients = append(clients, *conn)
 
-    // API endpoint to add a new message
-    router.HandleFunc("/messages", addMessage).Methods("POST")
+		// Send chat history to new client
+		for _, msg := range history {
+			if err := conn.WriteMessage(websocket.TextMessage, []byte(msg)); err != nil {
+				fmt.Printf("error sending message to client: %v\n", err)
+			}
+		}
 
-    // Websocket endpoint to handle real-time updates
-    router.HandleFunc("/ws", websocketHandler)
+		for {
+			// Read message from browser
+			msgType, msg, err := conn.ReadMessage()
+			if err != nil {
+				return
+			}
 
-    // Serve the frontend content
-    router.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-        http.ServeFile(w, r, "index.html")
-    })
+			// Print the message to the console
+			fmt.Printf("%s sent: %s\n", conn.RemoteAddr(), string(msg))
 
-    // Serve the main.js file
-  // Serve the main.js file
-router.HandleFunc("/client/main.js", func(w http.ResponseWriter, r *http.Request) {
-    http.ServeFile(w, r, "./client/main.js")
-})
+			// Add message to chat history
+			history = append(history, string(msg))
 
+			// Broadcast message to all clients
+			for _, client := range clients {
+				if err = client.WriteMessage(msgType, msg); err != nil {
+					return
+				}
+			}
+		}
+	})
 
-    log.Fatal(http.ListenAndServe(":8080", router))
-    log.Println("Server is running on port 8080")
-}
+	// Serve the index.html file
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, "index.html")
+	})
 
-func getMessages(w http.ResponseWriter, r *http.Request) {
-    json.NewEncoder(w).Encode(messages)
-}
+	// Serve the main.js script
+	http.HandleFunc("/client/main.js", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, "client/main.js")
+	})
 
-func addMessage(w http.ResponseWriter, r *http.Request) {
-    var message Message
-    _ = json.NewDecoder(r.Body).Decode(&message)
-    messages = append(messages, message)
-    json.NewEncoder(w).Encode(message)
-}
-
-func websocketHandler(w http.ResponseWriter, r *http.Request) {
-    // Upgrade the HTTP connection to a WebSocket connection
-    conn, err := upgrader.Upgrade(w, r, nil)
-    if err != nil {
-        log.Println(err)
-        return
-    }
-    
-    // Add the new connection to the list of connections
-    connections := make(map[*websocket.Conn]bool)
-    
-    defer func() {
-        // Close the WebSocket connection and remove it from the list of connections
-        conn.Close()
-        delete(connections, conn)
-    }()
-    
-    // Send the list of messages to the newly connected client
-    err = conn.WriteJSON(messages)
-    if err != nil {
-        log.Println(err)
-        return
-    }
-    
-    for {
-        // Listen for new messages and push them to connected clients
-        var message Message
-        err = conn.ReadJSON(&message)
-        if err != nil {
-            if websocket.IsUnexpectedCloseError(err, websocket.CloseGoingAway) {
-                log.Printf("error: %v", err)
-            }
-            break
-        }
-        
-        messages = append(messages, message)
-        
-        for conn := range connections {
-            err = conn.WriteJSON(messages)
-            if err != nil {
-                log.Println(err)
-                return
-            }
-        }
-    }
+	http.ListenAndServe(":8080", nil)
 }
