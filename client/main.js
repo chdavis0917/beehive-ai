aeac53652b65155b2023c3acffca01366b87fc19
chdavis0917 <chdavis0917@gmail.com>
2023-04-12 15:44:05 -0700

    messages can now be submitted when hitting enter

--- client/main.js
+++ client/main.js
@@ -3,14 +3,20 @@ let output = document.getElementById("output");
 let socket = new WebSocket("ws://localhost:8080/echo");
 
 socket.onopen = function () {
-    output.innerHTML += "Status: Connected\n";
+  output.innerHTML += "Status: Connected\n";
 };
 
 socket.onmessage = function (e) {
-    output.innerHTML += "Server: " + e.data + "\n";
+  output.innerHTML += "Server: " + e.data + "\n";
 };
 
 function send() {
-    socket.send(input.value);
-    input.value = "";
+  socket.send(input.value);
+  input.value = "";
+}
+
+function handleKeyPress(event) {
+  if (event.keyCode === 13) {
+    send();
+  }
 }
